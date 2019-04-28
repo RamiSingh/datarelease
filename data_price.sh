42fa4a17140654d2da543ade17ea8b364f77c653
rami.singh <raminderjit@outlook.com>
2019-04-28 22:12:06 +1000

    added functions for prompts

--- data_price.sh
+++ data_price.sh
@@ -28,32 +28,22 @@
 ##############################
 #   DATA RELEASE V1.0(WIP)   #
 ##############################
-
-
-# Let's get the locations saved in the form of variables:
-# For INT1 AU
-ROOT_DIR_INT1=/mnt/backup/volaxn_iau/nfs/int1/masterdata/    #This is the common path.
-PRICE_DIR_INT1=$ROOT_DIR_INT1/$COUNTRY/master/                  #Price files are dropped here by the data team.
-APWEB_INT1=$ROOT_DIR_INT1/$COUNTRY/apwebdata/
-AUDMOB_INT1=$ROOT_DIR_INT1/$COUNTRY/audamobile/                 #There is no audamobile for NZ; so need to figure out a way to avoid this for NZ.
-SRCHTREE_INT1_DATA=$ROOT_DIR_INT1/$COUNTRY/searchtree/           #The same files need to be copied to axn/config directory or else there will be issues.
-SRCHTREE_INT1_CNFG=/mnt/backup/volaxn_iau/nfs/int1/axn/config/$COUNTRY/searchtree/
-QPTR_DROP=$ROOT_DIR_INT1/$COUNTRY/Qapter                       #These files need to be copied to webpaddata folder under the correct directory structure.
-WBPD_INT1=$ROOT_DIR_INT1/$COUNTRY/webpaddata                    #Create a new directoy under this folder and copy the tips folder from the previous version folder.
-
-#For INT1 NZ
-#PRICE_DIR_NZ_INT1=$ROOT_DIR_INT1/$COUNTRY/master                   #Price files are dropped here by the data team.
-#APWEB_NZ_INT1=$ROOT_DIR_INT1/nz/apwebdata
-#AUDMOB_NZ_INT1=$ROOT_DIR_INT1/nz/audamobile
-#SRCHTREE_NZ_INT1_DATA=$ROOT_DIR_INT1/nz/searchtree           #The same files need to be copied to axn/config directory or else there will be issues.
-#SRCHTREE_NZ_INT1_CNFG=/mnt/backup/volaxn_iau/nfs/int1/axn/config/nz/searchtree
-#QPTR_DROP_NZ=$ROOT_DIR_INT1/nz/Qapter                           #These files need to be copied to webpaddata folder under the correct directory structure.
-#WBPD_NZ_INT1=$ROOT_DIR_INT1/nz/webpaddata                    #Create a new directoy under this folder and copy the tips folder from the previous version folder.
-
-# Now that the location specific variables are taken care of, let's find a way for the user to provide some way of input to distinguish between the role of this script.
+# let's find a way for the user to provide some way of input to distinguish between the role of this script.
 # I want the user to input firstly whether the script is being used to do a price file release or a data release. Also, I need the user to tell the script as to what country is it for and
 # what envrironment. I am thinking of creating fucntions for each of these tasks and then depending on the user input, calling a case to perform that function.
 # Firstly need to find out out what the user wants to do. Let's ask some questions:
+
+
+echo "What country you want to run this script for?"
+sleep 2
+echo "Options are: au or nz"
+sleep 2
+echo "Please enter one of the options from au/nz. Please use lower case only"
+
+# Let's read the input and save this in a variable country
+read COUNTRY
+echo "You have chosen $COUNTRY."
+
 
 echo "What do you want to do?"
 sleep 2
@@ -64,28 +54,66 @@ echo "For PriceFileUpdate, enter: PRICE"
 sleep 2
 echo "For DataRelease, enter: DATA"
 
-#NOTE: CREATE SIMILAR ECHO COMMANDS TO GET THE VERSIONS OF PRICE FILES AND DATA RELEASE#
-
 # User's iput will need to be stored in a variable for later use:
 read UINPT                     # This variable defines the task that the script is doing.
 echo "You have chosen a $UINPT release."
 
-# Now, we need to know what country does the user want to make the changes to. Again, asking some questions:
+#NOTE: CREATE SIMILAR ECHO COMMANDS TO GET THE VERSIONS OF PRICE FILES AND DATA RELEASE#
 
-echo "What country you want to run this script for?"
-sleep 2
-echo "Options are: au or nz"
-sleep 2
-echo "Please enter one of the options from au/nz"
+#Create a function for prompting for price file version:
 
-# Let's read the input and save this in a variable country
-read COUNTRY
-echo "You have chosen $COUNTRY."
+price_prompt()
+{
+      echo "Please enter the price file version in the format XXXX. e.g. 0075"
+      read NEW_PRICE
+}
+
+data_prompt_au()
+{
+      echo "Please enter the version of apwebdata/onepad"
+      sleep 1
+      read APW_AU
+
+      echo "Please enter the version of Audamobile"
+      Sleep 1
+      read AM_AU
+
+      echo "Please enter the version of Searchtree"
+      sleep 1
+      read ST_AU
+
+     #Qapter files need to be  copied over to the webpaddata folder before being updated.We will get the Qapter folder version and then move the files
+     #+ This is only for INT1. For Prod2 the files will be directly copied from INT1. Same goes for PROD.
+      echo "Please enter the version of Qapter"
+      sleep 1
+      read QAP_AU
+}
+
+
+
+# get the locations saved in the form of variables:
+# For INT1 AU
+ROOT_DIR_INT1=/mnt/backup/volaxn_iau/nfs/int1/masterdata/    #This is the common path.
+PRICE_DIR_INT1=$ROOT_DIR_INT1/$COUNTRY/master/                  #Price files are dropped here by the data team.
+APWEB_INT1=$ROOT_DIR_INT1/$COUNTRY/apwebdata/
+AUDMOB_INT1=$ROOT_DIR_INT1/$COUNTRY/audamobile/                 #There is no audamobile for NZ; so need to figure out a way to avoid this for NZ.
+SRCHTREE_INT1_DATA=$ROOT_DIR_INT1/$COUNTRY/searchtree/           #The same files need to be copied to axn/config directory or else there will be issues.
+SRCHTREE_INT1_CNFG=/mnt/backup/volaxn_iau/nfs/int1/axn/config/$COUNTRY/searchtree/
+QPTR_DROP=$ROOT_DIR_INT1/$COUNTRY/Qapter                       #These files need to be copied to webpaddata folder under the correct directory structure.
+WBPD_INT1=$ROOT_DIR_INT1/$COUNTRY/webpaddata                    #Create a new directoy under this folder and copy the tips folder from the previous version folder.
+
+
+
+
+
+
 
 #GET THE USER TO INPUT THE VERSIONS OF THE FILES AND CREATE FUNCTIONS THAT BASED ON $UINPT TO PRESENT FOR PRICE FILE OR DATA RELEASE VERSIONS.
 
+
 # Create a function that does the price file changes
 price() {
+
 #cd to the master directoy
 cd $PRICE_DIR_INT1
 #Save the value of lastversion into a variable. This will be the directory to copy old files from
