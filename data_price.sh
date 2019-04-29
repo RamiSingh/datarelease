8c5172acee33d136d41f73d2eabcec6fa175d4b9
RamiSingh <raminderjit@outlook.com>
2019-04-29 14:52:27 +1000

    Added environment specific questions and some PROD directories

--- data_price.sh
+++ data_price.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#NOTE: This script needs to be run as a root user. {Maybe I can add a test to this effect.}
+#NOTE: This script needs to be run as a root user or SUDO. {Maybe I can add a test to this effect.}
 
 # Q: Why do we need this script?
 # A: To enable data release in an automated and standard way so that there is no room for any errors. This script will entail all the steps/commands that are essential in
@@ -31,6 +31,28 @@
 #   DATA RELEASE V1.0(WIP)   #
 ##############################
 
+#INT1 specific directories:
+ROOT_DIR_INT1=/mnt/backup/volaxn_iau/nfs/int1/masterdata/    #This is the common path.
+PRICE_DIR_INT1=$ROOT_DIR_INT1/$COUNTRY/master/                  #Price files are dropped here by the data team.
+APWEB_INT1=$ROOT_DIR_INT1/$COUNTRY/apwebdata/
+AUDMOB_INT1=$ROOT_DIR_INT1/$COUNTRY/audamobile/                 #There is no audamobile for NZ; so need to figure out a way to avoid this for NZ.
+SRCHTREE_INT1_DATA=$ROOT_DIR_INT1/$COUNTRY/searchtree/           #The same files need to be copied to axn/config directory or else there will be issues.
+SRCHTREE_INT1_CNFG=/mnt/backup/volaxn_iau/nfs/int1/axn/config/$COUNTRY/searchtree/
+QPTR_DROP=$ROOT_DIR_INT1/$COUNTRY/Qapter                       #These files need to be copied to webpaddata folder under the correct directory structure.
+WBPD_INT1=$ROOT_DIR_INT1/$COUNTRY/webpaddata                    #Create a new directoy under this folder and copy the tips folder from the previous version folder.
+
+#PROD specific directories
+ROOT_DIR_PROD=/mnt/backup/volaxn_pau/nfs/masterdata/
+PRICE_DIR_PROD=$ROOT_DIR_PROD/$COUNTRY/master/
+APWEB_PROD=$ROOT_DIR_PROD/$COUNTRY/apwebdata/
+AUDMOB_PROD=$ROOT_DIR_PROD/$COUNTRY/audamobile/
+SRCHTREE_PROD_DATA=$ROOT_DIR_PROD/$COUNTRY/searchtree/
+SRCHTREE_PROD_CNFG=/mnt/backup/volaxn_pau/nfs/prod/axn/config/$COUNTRY/searchtree/
+WBPD_PROD=$ROOT_DIR_PROD/$COUNTRY/webpaddata/                          #There is no Qapter folder for PROD and PROD2. The files are transferred from INT1 webpaddata directly.
+
+
+
+
 # Now that the location specific variables are taken care of, let's find a way for the user to provide some way of input to distinguish between the role of this script.
 # I want the user to input firstly whether the script is being used to do a price file release or a data release. Also, I need the user to tell the script as to what country is it for and
 # what envrironment. I am thinking of creating fucntions for each of these tasks and then depending on the user input, calling a case to perform that function.
@@ -56,19 +78,32 @@ echo "For DataRelease, enter: DATA"
 read UINPT                     # This variable defines the task that the script is doing.
 echo "You have chosen a $UINPT release."
 
+# Ask user for the environment for which this script needs to run:
+
+echo "Which environment are you making the changes on?
+Your options are:
+INT
+PP
+PROD"
+echo "Please enter one of the above options"
+read ENV
+
+# The idea is to create functions for each of these environments. INT and PROD can be done using the same function but PP will need to be written separately as the file system structure will not allow for simple cp command.
+
+
 #NOTE: CREATE SIMILAR ECHO COMMANDS TO GET THE VERSIONS OF PRICE FILES AND DATA RELEASE#
 
 #Create a function for prompting for price file version:
 
 price_prompt()
-{
+    {
       echo "Please enter the price file version in the format XXXX. e.g. 0075"
-      read NEW_PRICE
-}
+      read NEW_PRICE     #This will be called by the price file update function.
+    }
 
 #The reason to use separate prompts for AU and NZ is that the Audamobile does not exist for NZ.
 data_prompt_au()
-{
+    {
       echo "Please enter the version of apwebdata/onepad"
       read APW_AU
 
@@ -82,10 +117,10 @@ data_prompt_au()
      #+ This is only for INT1. For Prod2 the files will be directly copied from INT1. Same goes for PROD.
       echo "Please enter the version of Qapter"
       read QAP_AU
-}
+    }
 
 data_prompt_nz()
-{
+    {
       echo "Please enter the version of apwebdata/onepad"
       read APW_NZ
 
@@ -97,7 +132,7 @@ data_prompt_nz()
      #+ This is only for INT1. For Prod2 the files will be directly copied from INT1. Same goes for PROD.
       echo "Please enter the version of Qapter"
       read QAP_NZ
-}
+    }
 
 #The functions to gather the version details are now done. Next, we need to use these functions in a case statement:
 
@@ -117,3 +152,18 @@ case "$UINPT" in
               fi
               ;;
 esac
+
+# PRICE FILE update function is probably the simpleast of these.
+
+price_update()
+{
+    case "$ENV" in
+
+      INT)
+      cd
+
+
+
+
+
+}
